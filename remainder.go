a41e03d210b0ec4deacfc23ee186b8c3ba656f4a
Leepod <caleb@csjtechnologies.com>
2016-11-17 01:29:02 -0500

    Create Remainder.go and use modulo to determine if a number is odd or even

---
+++ remainder.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	for x := 1; x >= 100; x++ {
+		if x%2 == 1 {
+			fmt.Printf("%d - is odd", x)
+		} else {
+			fmt.Printf("%d - is even", x)
+		}
+	}
+}
