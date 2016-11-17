d67fd0f9f7d5cdd865239a325c41dc613f5a6316
Leepod <caleb@csjtechnologies.com>
2016-11-17 01:33:27 -0500

    Less than 100 in the for loop and not equal to one using modulo

--- remainder.go
+++ remainder.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	for x := 1; x >= 100; x++ {
-		if x%2 == 1 {
+	for x := 1; x <= 100; x++ {
+		if x%2 != 1 {
 			fmt.Printf("%d - is odd", x)
 		} else {
 			fmt.Printf("%d - is even", x)
